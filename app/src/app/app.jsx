431b0c86f6b62c15f4f4ba0bebfa5816d1435ec7
Yong Li Dich <ydich@college.harvard.edu>
2020-01-24 19:46:42 -0800

    preliminary example API call into the ui :)

--- app/src/app/app.jsx
+++ app/src/app/app.jsx
@@ -12,16 +12,59 @@ class App extends Component {
         isLoaded: false, 
         items: []
     }; 
-    
+
+    // block comments in VSCode: SHIFT + ALT + A
+    /* componentDidMount in React: is executed after the first render only on the client side. 
+    This is where AJAX requests and DOM or state updates should occur. 
+    This method is also used for integration with other JavaScript 
+    frameworks and any functions with delayed execution such as setTimeout 
+    or setInterval. We are using it to update the state so we can trigger 
+    the other lifecycle methods. */
+    componentDidMount(){
+    axios.get("https://jsonplaceholder.typicode.com/users").then(
+      result => {
+        this.setState({
+          isLoaded: true,
+          items: result.data
+        });
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      error => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+      );
+    }
     render() {
-        return <div id="app-root"><h1>EMPACT</h1>
-            <form> 
-                <input type="text" />
-                <button id="learn" type="submit">Learn More</button>
-                <button type="submit">Donate</button>
-                <button type="submit">Volunteer</button>
-            </form>
-        </div>
+        const {error, isLoaded, items} = this.state; 
+        if (error) {
+            return <div> Error: {error.message} </div>
+        } else if (!isLoaded) {
+            return <div> Loading ... </div>; 
+        } else {
+            return(
+                <div id="app-root"><h1>EMPACT</h1>
+                <form> 
+                    <input type="text" />
+                    <button id="learn" type="submit">Learn More</button>
+                    <button type="submit">Donate</button>
+                    <button type="submit">Volunteer</button>
+                </form>
+                <ul>
+                    {items.map(item => (
+                        <li key={item.username}>
+                            {item.username}: {item.name}
+                        </li>
+                    ))}
+                </ul>
+                </div>
+            ); 
+        }
+        
     }
 }
 
